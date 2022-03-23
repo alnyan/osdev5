38c9fa783d920762dca89a25515ac970c739b20d
Mark Poliakov <mark@alnyan.me>
2022-03-23 17:41:50 +0200

    feature: x86_64 fuzzy revert

--- libusr/src/arch/x86_64.rs
+++ libusr/src/arch/x86_64.rs
@@ -2,42 +2,42 @@
 macro_rules! syscall {
     ($num:expr) => {{
         let mut res: usize = $num.repr();
-        asm!("syscall",
+        core::arch::asm!("syscall",
              inout("rax") res,
              options(nostack));
         res
     }};
     ($num:expr, $a0:expr) => {{
         let mut res: usize = $num.repr();
-        asm!("syscall",
+        core::arch::asm!("syscall",
              inout("rax") res, in("rdi") $a0,
              options(nostack));
         res
     }};
     ($num:expr, $a0:expr, $a1:expr) => {{
         let mut res: usize = $num.repr();
-        asm!("syscall",
+        core::arch::asm!("syscall",
              inout("rax") res, in("rdi") $a0, in("rsi") $a1,
              options(nostack));
         res
     }};
     ($num:expr, $a0:expr, $a1:expr, $a2:expr) => {{
         let mut res: usize = $num.repr();
-        asm!("syscall",
+        core::arch::asm!("syscall",
              inout("rax") res, in("rdi") $a0, in("rsi") $a1,
              in("rdx") $a2, options(nostack));
         res
     }};
     ($num:expr, $a0:expr, $a1:expr, $a2:expr, $a3:expr) => {{
         let mut res: usize = $num.repr();
-        asm!("syscall",
+        core::arch::asm!("syscall",
              inout("rax") res, in("rdi") $a0, in("rsi") $a1,
              in("rdx") $a2, in("r10") $a3, options(nostack));
         res
     }};
     ($num:expr, $a0:expr, $a1:expr, $a2:expr, $a3:expr, $a4:expr) => {{
         let mut res: usize = $num.repr();
-        asm!("syscall",
+        core::arch::asm!("syscall",
              inout("rax") res, in("rdi") $a0, in("rsi") $a1,
              in("rdx") $a2, in("r10") $a3, in("r8") $a4, options(nostack));
         res
